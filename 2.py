9660c7f38d9bb76236b79150e5b11a2b8b581b04
Dominika Pawlaczyk <pawlaczyk@g.pl>
2016-02-26 10:18:30 +0100

    Pierwsze zajecia skonczone

--- 2.py
+++ 2.py
@@ -23,7 +23,7 @@ ZADANIE DOMOWE:
 """
 
 
-#ad1.
+#ad.1
 def ad1():
 	base = ["Ala", 1,233, "kot", {"PI": 3.14}, "e", 2.7183, "Mruczyslaw"]
 	print base
@@ -32,10 +32,10 @@ def ad1():
 	baseRev = sorted(base, reverse = True)
 	print baseRev
 
-#ad2.
+#ad.1
 #coding=utf-8
 
-#ad3.
+#ad.3
 def ad3():
 	n = 0
 	d = {"Ala":"kotek", "PI":3.14, 0: "zero", 1:1, 2.7183: "e", n : None}
@@ -61,8 +61,8 @@ def ad3():
 	d.clear()
 	print d
 
-#ad4.
-	def ad4():
+#ad.4
+def ad4():
 	myfile = open("Text.txt", "w")
 	myfile.write('Ala ma kota\n')
 	myfile.write("Jednak nie\nBo to kot ma Ale\n <3")
@@ -106,4 +106,151 @@ def ad3():
 	binarData = open('data.bin', 'rb')
 	print binarData.read()
 
- 
+#ad.5
+#serializacja obiektow typu pythonowego i instancji klas 
+
+def ad5():
+	import pickle
+	"""Brak danych na temat roznicy miedzy dump vs dumps oraz load vs loads"""
+
+	data = { "PI":3.14, "e":2.7183, "black":(255, 255, 255), 1:1, "kot":"Puszek"}
+	#dump - zapisywanie do pliku
+	pickle.dump(data, open("save.p", "wb"))
+
+	#load - zaladowanie z pliku 
+	data = pickle.load(open("save.p", "rb"))
+	print data
+
+	#klasa ktorej obiekt sobie zapiszemy
+	class Example():
+		def __init__(self, name, surname): 
+			self.name = name
+			self.surname = surname
+		def __str__(self):
+			return str(self.name, self.surname)
+
+
+	with open("object.p", "wb") as output:
+		e = Example("Name", "Surname")
+		pickle.dump(e, output, pickle.HIGHEST_PROTOCOL)
+	del e
+
+	with open("object.p", "rb") as input:
+		e = pickle.load(input)
+	
+
+
+########################################################################
+#zadania domowe
+
+def ZadDomAd2():
+	#ad1 Uwaga bardzo brzydki niepythonowy kod no ale dziala
+	l_krotek = [(1,), (255,255,255),("Ala", "kotek"), ("e", 2.7182),(3,3), (0,2,4,6,8,10, 12)]
+	liczby = [x for x in l_krotek for j in x ]
+	#print str(l_krotek[1][-1]).isdigit()
+
+	def liczbyZlisty(l_krotek):
+		l2 = []
+		flaga = 0
+		for i in l_krotek:
+			suma = 0
+			for j in i:
+				if str(j).isdigit() :
+					flaga = 1 
+				else:
+					flaga = 0
+			if flaga == 1:
+				l2.append((i))
+				flaga = 0
+		return l2
+			
+	l2 = liczbyZlisty(l_krotek)
+	print l2
+
+	#ad1.1
+	l3 = []
+	for i in l2:
+		suma = 0
+		for j in i:
+			suma+=j
+		if(suma % 2 == 0):
+			l3.append((i))
+			
+	print "l3: ",l3
+
+	#ad1.2
+	l4= []
+	flaga = 0
+	for i in l2:
+		for j in i:
+			if j % 3 == 0:
+				flaga = 1
+			else:
+				flaga = 0
+				break
+				
+		if flaga == 1:
+			l4.append((i))
+			flaga = 0
+	print "l4: ",l4
+
+	#ad1.3
+	l5 = []
+	f = 0
+	for i in l2:
+		for j in i:
+			if j % 3 == 0:
+				f = 1
+				break
+		if f == 1:
+			l5.append((i))
+			f = 0
+			
+	print l5
+	
+##########################################
+def ZadDomAd2():
+	#ad2
+	stringi = "0 Informatyk sie 3.14 cieszy; 255,255,255 gdy sie -123 uczy o stringach 2.7182 ".split()
+	print stringi
+	#ad2.1
+	sameStringi = [x for x in stringi if x.isalpha()]
+	print sameStringi
+	#ad2.2
+	print stringi
+	print str(stringi[3]).isdigit()
+
+	sameLiczby = [x for x in stringi if x.isalpha() == False]
+
+	#trololo isdigit nie dziala dla FLOAT <----- Error Trololo Python
+	sL = []
+	for i in sameLiczby:
+		if i.replace('.','').replace('-','').isdigit():
+			sL.append(i)
+
+	print sameLiczby
+	print sL
+	sl =[int(float(x)) for x in sL] #nie moge od razu zamienic na float :<
+	print sl
+
+#################################3
+#ad.3
+def ad3():
+	import re
+	def method2(txt):
+		return '\n'.join([x for x in txt.split("\n") if x.strip()!=''])
+
+	z = ''
+	l = []
+
+	with open("2.txt", "r" ) as file:
+		for line in file:
+			line = line.partition('#')[0]
+			line = line.rstrip()
+			if re.match(r'^\s*$', line): 
+				line = ''
+			else:
+				l.append(line + '\n')
+	file.close()
+
+	print l
